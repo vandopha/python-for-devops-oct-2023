175b86eedfc484e5d33ac40d260f445d3f3d87b1
vandopha <venderhak@gmail.com>
2023-10-19 13:16:39 +0700

    install something

---
+++ vandopha/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vandopha/Documents/python_practices/python-for-devops-oct-2023/vandopha/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
